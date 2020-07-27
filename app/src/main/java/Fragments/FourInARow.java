240f2c410e4541960bf47b70fd8d1c014d01f170
Mohammad Dadsetani <mo.dadsetani@gmail.com>
2020-07-27 13:30:57 +0430

    Initial commit

---
+++ app/src/main/java/Fragments/FourInARow.java
@@ -0,0 +1,30 @@
+package Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.twoinonegame.R;
+
+public class FourInARow extends Fragment {
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_four_in_a_row, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/twoinonegame/Controller/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.twoinonegame.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import com.example.twoinonegame.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = new Fragment();
+        
+    }
+}
